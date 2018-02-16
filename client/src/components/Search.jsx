878e869d72604dff4d7698246bcd451564c6f716
Zachary <zinouye@gmail.com>
2018-02-16 14:55:54 -0800

    fixed search bug

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,20 +9,22 @@ class Search extends React.Component {
   }
 
   onChange (e) {
+    console.log(e.target.value)
     this.setState({
       term: e.target.value
     });
   }
 
   search() {
+    console.log(this.state.term);
     this.props.onSearch(this.state.term);
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange.bind(this)}/>       
+      <button onClick={this.search.bind(this)}> Add Repos </button>
     </div>) 
   }
 }
